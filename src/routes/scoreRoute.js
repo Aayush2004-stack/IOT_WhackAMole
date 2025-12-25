686be37afe3eac42cdb52642b80463466f8ce009
Aayush <aayushbastola772@gmail.com>
2025-12-25 23:27:32 +0545

    highest score route

--- src/routes/scoreRoute.js
+++ src/routes/scoreRoute.js
@@ -4,6 +4,7 @@ const ctrl= require("../controller/scoreController");
 
 const router = express.Router();
 
-router.post("/",ctrl.addScore)
+router.post("/",ctrl.addScore);
+router.get("/highScore",ctrl.getHighScore);
 
 module.exports= router;
