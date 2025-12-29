2dde5e3f79d377ea944b28888954b91113fa0398
Aayush <aayushbastola772@gmail.com>
2025-12-29 22:28:52 +0545

    commented save fianl score api

--- src/controller/scoreController.js
+++ src/controller/scoreController.js
@@ -1,22 +1,22 @@
 const scoreService = require("../service/scoreService");
 
-exports.addScore = async (req, res) => {
-  try {
-    const { score } = req.body;
+// exports.addScore = async (req, res) => {
+//   try {
+//     const { score } = req.body;
 
-    if (!score) {
-      return res.status(400).json({ message: "No score provided" });
-    }
-    scoreService.saveFinalScore(score);
-    return res.status(200).json({
-      message: "score added successfully",
-    });
-  } catch (err) {
-    return res.status(500).json({
-      message: err,
-    });
-  }
-};
+//     if (!score) {
+//       return res.status(400).json({ message: "No score provided" });
+//     }
+//     scoreService.saveFinalScore(score);
+//     return res.status(200).json({
+//       message: "score added successfully",
+//     });
+//   } catch (err) {
+//     return res.status(500).json({
+//       message: err,
+//     });
+//   }
+// };
 
 exports.getHighScore = async (req, res) => {
   const highScore = await scoreService.getHighScore();
